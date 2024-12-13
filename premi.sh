3745b807c82fb37f230a105d51462722743e1e74
aisahblackmarket <aisahaisah3545@gmail.com>
2024-12-13 20:02:29 +0700

    Add files via upload

--- premi.sh
+++ premi.sh
@@ -30,10 +30,10 @@ clear;clear;clear
 
   # // Banner
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
-echo -e "  Welcome To RIFQI Tunneling ${YELLOW}(${NC}${green} Stable Edition ${NC}${YELLOW})${NC}"
+echo -e "  Welcome To AdijayaStoreVPN Tunneling ${YELLOW}(${NC}${green} Stable Edition ${NC}${YELLOW})${NC}"
 echo -e " This Will Quick Setup VPN Server On Your Server"
-echo -e "  Auther : ${green}® ${NC}${YELLOW}(${NC} ${green} RIFQI Tunneling ${NC}${YELLOW})${NC}"
-echo -e " © Recode By My Self RIFQI Tunneling${YELLOW}(${NC} 2023 ${YELLOW})${NC}"
+echo -e "  Auther : ${green}® ${NC}${YELLOW}(${NC} ${green} AdijayaStoreVPN Tunneling ${NC}${YELLOW})${NC}"
+echo -e " © Recode By My Self AdijayaStoreVPN Tunneling${YELLOW}(${NC} 2025 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 sleep 2
